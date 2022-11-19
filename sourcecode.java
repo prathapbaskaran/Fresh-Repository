4e2e8ee9eaec4b9ee2e07d49453763adb742431e
“Prathap <prathapbaskaran12121996@gmail.com>
2022-11-19 10:42:33 +0000

    fourth commit

--- sourcecode.java
+++ sourcecode.java
@@ -4,7 +4,7 @@ import java.io.printer;
 import java.io.collection; 
 import java.io.scanner;
 import java.io.outputprinter;
-import java.io.Inputfiles;
+import java.io.Vijay;
 public class SpiralPatternExample2
 {  
 //function to print the spiral pattern  
